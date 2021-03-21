bdc57050a63b61be579e30c0ca36d93e90afc4a4
Scotty Test <scottmvau@hotmail.com>
2021-03-21 23:12:19 +1000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by MrMacLappy on 21/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
